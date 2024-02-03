c724f519faf736169418d663def34aeb813f418c
DoctorDenis <sychovdenis@icloud.com>
2024-02-03 17:15:12 +0200

    Changed starting page to plug page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,18 +5,8 @@ import styles from "./page.module.css";
 export default function Home() {
   return (
     <main className={styles.main}>
-      <h1 className={styles.header}>TEATR ILUZJI ALEKSANDRA TKACZA</h1>
-      <p className={styles.text}>
-        Spektakl ''Tajemnice Iluzji'' jest przygotowany z myślą o dzieciach i
-        młodzieży. Forma spektaklu jest bardzo atrakcyjna, dopasowana do poziomu
-        percepcji uczniów, pobudza również aktywność intelektualną uczestników
-        przedstawienia oraz daje interesujący pogląd na matematykę, fizykę
-        poprzez sztukę iluzji. Współudział uczniów w rozwiązywaniu zadań w
-        trakcie spektaklu wywiera pozytywny wpływ na tworzenie aktywnych i
-        twórczych postaw. Sztuka iluzji dostarcza miłych wrażeń, niezapomnianych
-        przeżyć oraz korzystnie wpływa 6 na rozwój wyobraźni i logicznego
-        myślenia.
-      </p>
+      <span className={styles.header}>Przepraszam. Strona w budowie</span>
+      span
     </main>
   );
 }
